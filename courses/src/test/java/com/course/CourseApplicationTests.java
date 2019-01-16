1c1df54c68b13362d5f9976d4b2c2e9d00ec8f13
JohnAssebe <39563000+JohnAssebe@users.noreply.github.com>
2019-01-16 17:35:57 +0300

    third Commit
    Modified

--- courses/src/test/java/com/course/CourseApplicationTests.java
+++ courses/src/test/java/com/course/CourseApplicationTests.java
@@ -1,16 +1,37 @@
 package com.course;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import com.course.Controller.HomeController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebMvcTest(HomeController.class)
 public class CourseApplicationTests {
-
+    @Autowired
+    private MockMvc mockMvc;
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws Exception {
+        mockMvc.perform(get("/"))    // <3>
+
+                .andExpect(status().isOk())  // <4>
+
+                .andExpect(view().name("course"))  // <5>
+
+                .andExpect(content().string(           // <6>
+                        containsString("Mathmatics is the ...")));
     }
 
 }
